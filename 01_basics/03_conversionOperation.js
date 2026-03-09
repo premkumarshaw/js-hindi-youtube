2705b1072586ebd0d07c282b3a36088bcb01b745
Prem Kumar Shaw <ps690864@gmail.com>
2026-03-09 18:50:22 +0000

    conversions of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,42 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score); //typecasting or type conversion
+console.log(typeof valueInNumber); //it is converted in Number but it is (NaN) Not a Number
+
+console.log(valueInNumber); //NaN
+
+//Notes-----------
+//If the below values are converted in Number, what will come
+//"33" => 33
+//"33abc" => NaN
+// true => 1;  and false => 0
+//"hitesh" => NaN (not a number)
+
+console.log("\n");//It will print a next line
+
+let isLoggedIn = 1 //number
+
+let booleanIsLoggedIn = Boolean(isLoggedIn) //typecasting in Boolean datatype
+
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// if they are converted in Bollean
+//1 => true
+//0 => false
+//"" (empty string) => false
+//"hitesh" = true
+
+
+console.log("\n");
+let someNumber = 33 //number
+
+let stringNumber = String(someNumber) //string
+
+console.log(stringNumber); //33
+console.log(typeof stringNumber); //type = string
+
+
